88f3980f370846e89f24fc82a3641f087429e14a
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2025-08-11 11:31:25 +0200

    refactor(api): remove AuthError import

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError } from 'axios';
-import { AuthResponse, AuthError, LoginCredentials, RegisterCredentials } from '../../types/auth';
+import { AuthResponse, LoginCredentials, RegisterCredentials } from '../../types/auth';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000',
